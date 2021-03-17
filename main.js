a51638e24f214240ac901225077733db3ea1f890
furik88 <sattorov.furkat88@gamil.com>
2021-03-17 23:45:39 +0500

    1

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function clickURLLanguage() {
 function changeLanguage() {
   let hash = window.location.hash
   hash = hash.substr(1)
-  console.log(hash);
+  
   if (!allLang.includes(hash)){
     location.href = window.location.pathname + '#ru'
     location.reload()
