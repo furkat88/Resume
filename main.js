e4e348669bab33613734d1078ca5cdc8646da33d
furik88 <sattorov.furkat88@gamil.com>
2021-03-17 22:26:18 +0500

    3

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
+const langArray = {
+  "h1":{
+    "ru": "Сатторов Фуркат",
+    "en": "Sattorov Furkat"
+  },
+  "main_aim_h2":{
+    "ru": "ЖЕЛАЕМАЯ ДОЛЖНОСТЬ",
+    "en": "CAREER POSITION"
+  },
+  "main_aim_p":{
+    "ru": "Веб-разработчик",
+    "en": "Web Developer"
+  }
+}
+const ruLang = document.querySelector('change_lang')
+const enLang = document.querySelector('change_lang2')
 
+ruLang.addEventListener('click', clickURLLanguage);
+
+// переноправить на URL с указанием языка 
+function clickURLLanguage() {
+  let lang = ruLang.value;
+  location.href = window.location.pathname + '#'+lang;
+  
+}
+console.log(lang);
